1b7d7a185ebb26ade4bcb30d811e233073849c9c
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-03-15 14:57:17 +0600

    dynamically issues loaded

--- scripts/main.js
+++ scripts/main.js
@@ -5,13 +5,16 @@ const allIssuesContainer = document.getElementById("all-issuesContainer")
 // shob issues er jonno array 
 let allIssues = [];
 
-// get all issues from API
-async function loadAllIssues(){
+// button gula load er jonno 
+async function loadAllButtons(){
+    
     const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
     const data = await res.json();
     console.log(data.data);
 
     allIssues = data.data;
+
+    btnsContainer.innerHTML = "";
 
     const filteredBtn = [];
     allIssues.forEach(issue =>{
@@ -33,7 +36,7 @@ async function loadAllIssues(){
     });
 };
 
-// btn click a 
+// btn click a je function ta ashbe  
 const selectBtnCategory = async (categoryStatus, btn)=>{
 
     const allTypeBtn = document.querySelectorAll("#btns-Container button, #allIssueBtn");
@@ -47,14 +50,19 @@ const selectBtnCategory = async (categoryStatus, btn)=>{
     btn.className = "active-btn w-[120px] py-2 px-3 rounded-sm";
 
     // API fetch 
-    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${categoryStatus}`);
+
+    // issue er status jodi 'categoryStatus hoy'
+    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues?data=${categoryStatus}`);
     const data = await res.json();
-    console.log(data.data);
+
+    console.log(data);
+    // display issue function call
+    displayAllIssues(data.data);
 }
 
 //loading spinner related
-
 const loadingSpinner = document.getElementById("loading-spinner");
+
 const showLoading = () => {
     loadingSpinner.classList.remove("hidden");
     loadingSpinner.classList.add("flex");
@@ -66,7 +74,78 @@ const hideLoading = () => {
     loadingSpinner.classList.remove("flex");
     loadingSpinner.classList.add("hidden");
 }
-loadAllIssues();
+
+//shob gula issue load korar jonno 
+async function loadAllIssues() {
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+    const jData = await res.json();
+
+    // shob issue display korar jonno
+    displayAllIssues(jData.data);
+}
+
+const displayAllIssues = (issues) =>{
+    console.log(issues);
+    allIssuesContainer.innerHTML ="";
+
+    issues.forEach(issue =>{
+        const issueCard = document.createElement("div");
+
+        // priority type er moddhe conditional rendering 
+        let priorityTypeClass = "";
+        if(issue.priority === "high"){
+            priorityTypeClass = "bg-[#FEECEC] text-[#EF4444]";
+        }
+        else if(issue.priority === "medium"){
+            priorityTypeClass = "bg-[#FFF6D1] text-[#F59E0B]"
+        }
+        else if(issue.priority === "low"){
+            priorityTypeClass = "bg-[#EEEFF2] text-[#9CA3AF]"
+        }
+
+        issueCard.className = "shadow h-full w-full";
+        issueCard.innerHTML = `
+            <div class="border-t-[3px] ${issue.status === "open"? "border-t-[#00A96E]" : "border-t-[#A855F7]"} p-4 rounded-sm space-y-4">
+
+                <div class="flex justify-between items-center">
+
+                    <span><img src="${issue.status === "open" ? './assets/open-status.png' : './assets/closed-status.png'}" alt="Status-Image"></span>
+                        <span class="rounded-[100px] ${priorityTypeClass} py-1.5 px-[25px]">
+                                        ${issue.priority.toUpperCase()}
+                        </span>
+
+                </div>
+
+                <div class = "space-y-2">
+                    <h3 class="font-semibold text-[14px]">${issue.title}</h3>
+                        <p class="text-[#64748B] text-[12px] line-clamp-2">
+                            ${issue.description}
+                        </p>
+                </div>
+                <div class="flex justify-start items-center gap-1">
+                    <button class="py-1.5 px-2 flex justify-start items-center text-[#EF4444] bg-[#FEECEC] text-[12px] rounded-[100px] border border-[#EF4444]/30 gap-0.5">
+                        <span class=""><img src="./assets/bug.png" alt=""></span>
+                         BUG
+                    </button>
+
+                    <button class="py-1.5 px-2 flex justify-start items-center text-[#D97706] bg-[#FFF8DB] text-[12px] rounded-[100px] border border-[#D97706]/30 gap-0.5">
+                    <span><img src="./assets/help-wanted.png" alt=""></span>
+                     HELP WANTED
+                    </button>
+                </div>
+
+            </div>
+            <div>
+                <div class="border-t border-[#E4E4E7] p-4 text-[12px] space-y-2 text-[#64748B]">
+                    <p>#${issue.id} by ${issue.author}</p>
+                    <p>${issue.createdAt}</p>
+                </div>
+            </div>
+        `;
+        allIssuesContainer.appendChild(issueCard);
+    })
+}
+loadAllButtons();
 
 
 
